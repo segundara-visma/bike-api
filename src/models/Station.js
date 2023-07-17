cc3074c49cb9a9ce70e6a0b801738028c189dfcd
segundara-visma <olusegun.okedara@visma.com>
2023-07-17 22:42:15 +0300

    feat: added trip and station models

---
+++ src/models/Station.js
@@ -0,0 +1,72 @@
+// Vendors
+import { DataTypes } from 'sequelize'
+// Sequelize instance
+import {sequelize} from '../utilities/Database.js'
+// Models
+import {Trip} from './Trip.js'
+
+const Station = sequelize.define('stations',
+  {
+    id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING(255)
+    },
+    address: {
+      type: DataTypes.STRING(255)
+    },
+    city: {
+      type: DataTypes.STRING(255)
+    },
+    operator: {
+      type: DataTypes.STRING(255)
+    },
+    capacity: {
+      type: DataTypes.INTEGER(11)
+    },
+    xCoord: {
+      type: DataTypes.DOUBLE
+    },
+    yCoord: {
+      type: DataTypes.DOUBLE
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    modifiedAt: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      allowNull: true
+    }
+  },
+  {
+    underscored: true,
+    paranoid: true,           // Soft delete.
+    timestamps: true,         // Must be true if there is soft delete (paranoid).
+    createdAt: 'created_at',  // Custom names for timestamp fields.
+    updatedAt: 'modified_at',
+    deletedAt: 'deleted_at',
+    tableName: 'stations'
+  })
+
+  Station.hasMany(Trip, {
+    as: 'departureTrips',
+    foreignKey: 'departure_station_id'
+  });
+  Trip.belongsTo(Station, { foreignKey: 'departure_station_id', as: 'departureTrips', onDelete: 'CASCADE' });
+
+  Station.hasMany(Trip, {
+    as: 'returnTrips',
+    foreignKey: 'return_station_id'
+  });
+  Trip.belongsTo(Station, { foreignKey: 'return_station_id', as: 'returnTrips', onDelete: 'CASCADE' });
+
+export {Station}
---
+++ src/models/Trip.js
@@ -0,0 +1,69 @@
+// Vendors
+import { DataTypes } from 'sequelize'
+// Sequelize instance
+import {sequelize} from '../utilities/Database.js'
+
+const Trip = sequelize.define('trips',
+  {
+    id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    departure: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    return: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    departureStationName: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    departureStationId: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true
+    },
+    returnStationName: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    returnStationId: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true
+    },
+    coveredDistance: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true
+    },
+    duration: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    modifiedAt: {
+      type: DataTypes.DATE,
+      allowNull: true
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      allowNull: true
+    }
+  },
+  {
+    underscored: true,
+    paranoid: true,           // Soft delete.
+    timestamps: true,         // Must be true if there is soft delete (paranoid).
+    createdAt: 'created_at',  // Custom names for timestamp fields.
+    updatedAt: 'modified_at',
+    deletedAt: 'deleted_at',
+    tableName: 'trips'
+  })
+
+  export {Trip}
