6f133afb9e645b84cdd3b137ea5fa458fd12eef4
segundara-visma <olusegun.okedara@visma.com>
2023-07-18 14:10:28 +0300

    docs: added env variable for TRANSLATION_DIR

--- src/config/app.js
+++ src/config/app.js
@@ -1,8 +1,3 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 export const grapqlConfig = {
   graphiql: true
 }
@@ -11,7 +6,7 @@ export const i18nConfig = {
   load: 'currentOnly', // Only load languages specified in supportedLngs.
   keySeparator: '.',
   backend: {
-    loadPath: __dirname + '/../static/i18n/{{lng}}.json' // Where to load language files. Relative to this file path.
+    loadPath: process.env.TRANSLATION_DIR + '/{{lng}}.json' // Where to load language files. Relative to this file path.
   },
   interpolation: {
     escapeValue: false // Do not escape variables.
